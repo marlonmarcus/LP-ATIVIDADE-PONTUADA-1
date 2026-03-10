59c02f0d3b5693ca91491753d8c469ec3bbcf697
Marlon Marcus <marcusmarlon25@gmail.com>
2026-03-10 20:32:41 -0300

    Update avaliativa-questao2.py
    consertei pequenos erros de escrita.

--- avaliativa-questao2.py
+++ avaliativa-questao2.py
@@ -1,9 +1,10 @@
 nome = input("Digite um seu nome: ")
-sexo = input("Digite seu sexo (masculino ou feminino):")
+sexo = input("Digite seu sexo (Masculino ou Feminino):")
 estado_civil = input("Digite seu estado civil: ")
 
 
 if sexo == "Feminino" and estado_civil == "Casada":
     anos_casado = input("Você tem quantos anos casada?")
 
-print(f"Seu nome é {nome}, seu sexo é {sexo}, seu estado civil é {estado_civil} e você tem {anos_casado} de casada.")
+
+print(f"Seu nome é {nome}, seu sexo é {sexo}, seu estado civil é {estado_civil} e você tem {anos_casado}anos de casada.")
