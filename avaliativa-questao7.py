95278aeac1239f06aa3b40088bc06674fbe24cd5
Marlon Marcus <marcusmarlon25@gmail.com>
2026-03-10 21:18:30 -0300

    Update avaliativa-questao7.py
    atualizando desconto

--- avaliativa-questao7.py
+++ avaliativa-questao7.py
@@ -5,16 +5,17 @@ total = quantidade_produto * preço_produto
 
 if quantidade_produto <= 5:
     print("Você recebeu um desconto de 2%")
-    desconto = 2/100
+    desconto = total * 2/100
     total_pagar = total - desconto
     print(f"Total a pagar: {total_pagar}")
 if quantidade_produto > 5 and quantidade_produto <= 10:
     print("Você recebeu um desconto de 3%")
-    desconto = 3/100
+    desconto = total * 3/100
     total_pagar = total - desconto
     print(f"Total a pagar: {total_pagar}")
 if quantidade_produto >10:
     print("Você recebeu 5% de desconto")
-    desconto = 5/100
+    desconto = total * 5/100
     total_pagar = total - desconto
+
     print(f"Total a pagar: {total_pagar}")
